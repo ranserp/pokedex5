cf14c9c9a709f831abb651f7e6fc06f0b0e02435
ranserp <30857015+ranserp@users.noreply.github.com>
2017-11-04 18:36:05 -0700

    Initial Commit

---
+++ pokedex5/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pokedex5
+//
+//  Created by alexander Presnar on 11/4/17.
+//  Copyright © 2017 alexander Presnar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
